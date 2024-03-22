bee48ea8bc25c9ba554bac5f518e4fd4957aa1c3
Jaydeny-png <jyau1020@gmail.com>
2024-03-22 10:13:15 -0400

    Added to java script array

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
+const namesArray = []
+
 function addName() {
-    var nameInput = document.getElementById("nameInput");
-    var name = nameInput.value.trim(); // Trim removes leading and trailing whitespace
-    if (name !== "") {
-      var table = document.getElementById("nameTable");
-      var row = table.insertRow(-1); // Insert a new row at the last position
-      var cell = row.insertCell(0); // Insert a cell in the row
-      cell.textContent = name; // Set the cell content to the input name
-      nameInput.value = ""; // Clear the input field after adding
-    } else {
-      alert("Please enter a name.");
-    }
-  }
+  const nameInput = document.getElementById('nameInput')
+  const name = nameInput.value.trim()
+
+  namesArray.push(name)
+  displayNames()
+
+  nameInput.value = ''
+}
