e7fb5bbbd2064227ba3337c250e7aab313ebeb1d
Jaydeny-png <jyau1020@gmail.com>
2024-03-25 11:55:02 -0400

    Made slightly nicish

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+// create an array to store names
 const namesArray = []
 
 function addName() {
-  const nameInput = document.getElementById('nameInput')
-  const name = nameInput.value.trim()
-
-  namesArray.push(name)
-  displayNames()
-
-  nameInput.value = ''
+    const nameInput = document.getElementById('nameInput') // storing name input element as a variable
+    const name = nameInput.value.trim() //get the TRIMMED value of the input box and store in a var
+    
+    namesArray.push(name) //add the name to the end of the array
+    displayNames() //call the displayNames function to update the list
+    
+    nameInput.value = '' //Clear the input field after adding the name
 }
+
+function displayNames() {
+    const nameList = document.getElementById('nameList')
+    nameList.innerHTML = '' //clear out the previous list items
+
+    for (let i = 0; i < namesArray.length; i++){
+        const name = namesArray[i]
+
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+
+        const span = document.createElement('span')
+        span.textContent = name
+
+        li.appendChild(span)//adds a span tag nested between every <li> tag (the child)
+        nameList.appendChild(li)
+    }
+
+}
+//Event listener for the button click to add a name
+document.getElementById('addNameBtn').addEventListener('click', addName)
